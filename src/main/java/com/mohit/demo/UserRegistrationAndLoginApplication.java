f7e5e394e6614c79040e1dc60ccff170d70a449f
Mohit Gidwani <mohit22gidwani@gmail.com>
2021-12-28 20:49:08 +0530

    finished project

---
+++ src/main/java/com/mohit/demo/UserRegistrationAndLoginApplication.java
@@ -0,0 +1,13 @@
+package com.mohit.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserRegistrationAndLoginApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserRegistrationAndLoginApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mohit/demo/UserRegistrationAndLoginApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mohit.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserRegistrationAndLoginApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
